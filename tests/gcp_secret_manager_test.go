78b6df1671865fe7aa92c767880507da9d1d42f4
harshmohansason <harshsason2000@gmail.com>
2025-06-21 08:28:35 +0530

    versionerror

--- tests/gcp_secret_manager_test.go
+++ tests/gcp_secret_manager_test.go
@@ -9,7 +9,9 @@ import (
 )
 
 func TestGetSecretFromGCP(t *testing.T){
+	
 	secretPath := fmt.Sprintf("projects/%s/secrets/%s/versions/latest",os.Getenv("GCP_PROD_PROJECT_ID"),"TWILIO_RECIPENTS_PHONE")
+	
 	secret, err := shared.GetSecretFromGCP(secretPath)
 	if err != nil{
 		t.Fatalf("Error occurred while getting the secret %v", err)
