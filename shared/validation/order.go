051dc71b4fe7c3b75e5af3f56d8f6dbcd16077a8
harshmohansason <harshsason2000@gmail.com>
2025-08-02 04:29:40 +0530

    Order validation moved to here completely to avoid import cycles

--- shared/validation/order.go
+++ shared/validation/order.go
@@ -8,13 +8,32 @@ import (
 	"github.com/HarshMohanSason/AHSChemicalsGCShared/shared/models"
 )
 
-// ValidateSpecialInstructions checks that the special instructions are safe plain text without any HTML or script-like content.
-func ValidateSpecialInstructions(o *models.Order) error {
-	if o.SpecialInstructions == "" {
+func ValidateOrder(o *models.Order) error {
+	if o.Uid == "" {
+		return errors.New("No user id found when order was placed")
+	}
+	if len(o.Items) == 0 {
+		return errors.New("No items found in order")
+	}
+	if o.TaxRate == 0 {
+		return errors.New("No tax rate found in order")
+	}
+	if o.Customer.ID == "" {
+		return errors.New("No customer found for this order")
+	}
+	if o.Customer.Name == "" {
+		return errors.New("No customer name found for this order")
+	}
+	return validateSpecialInstructions(o.SpecialInstructions)
+}
+
+// validateSpecialInstructions checks that the special instructions are safe plain text without any HTML or script-like content.
+func validateSpecialInstructions(specialInstructions string) error {
+	if specialInstructions == "" {
 		return nil
 	}
 	// Removing excess whitespace
-	instructions := strings.TrimSpace(o.SpecialInstructions)
+	instructions := strings.TrimSpace(specialInstructions)
 
 	// Reject if there are angle brackets (which might indicate HTML tags) or other unusual symbols
 	illegalPattern := regexp.MustCompile(`[<>[\]{}$%^*~|\\]`)
