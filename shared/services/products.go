55185a3b62155d77cd8f9060542b384c99210db1
harshmohansason <harshsason2000@gmail.com>
2025-08-03 04:31:01 +0530

    GetUpdatedProductDetails added to receive map of changed values b/w original and recieved new customer from quickbooks

---
+++ shared/services/products.go
@@ -0,0 +1,65 @@
+package services
+
+import (
+	"time"
+
+	"github.com/HarshMohanSason/AHSChemicalsGCShared/shared/models"
+)
+
+func GetUpdatedProductDetails(newProduct, oldProduct *models.Product) map[string]any {
+	if newProduct == nil || oldProduct == nil {
+		return nil
+	}
+
+	changedValues := make(map[string]any)
+
+	if newProduct.IsActive != oldProduct.IsActive {
+		changedValues["isActive"] = newProduct.IsActive
+	}
+	if newProduct.Brand != oldProduct.Brand {
+		changedValues["brand"] = newProduct.Brand
+	}
+	if newProduct.Name != oldProduct.Name {
+		changedValues["name"] = newProduct.Name
+	}
+	if newProduct.SKU != oldProduct.SKU {
+		changedValues["sku"] = newProduct.SKU
+	}
+	if newProduct.Size != oldProduct.Size {
+		changedValues["size"] = newProduct.Size
+	}
+	if newProduct.SizeUnit != oldProduct.SizeUnit {
+		changedValues["sizeUnit"] = newProduct.SizeUnit
+	}
+	if newProduct.PackOf != oldProduct.PackOf {
+		changedValues["packOf"] = newProduct.PackOf
+	}
+	if newProduct.Hazardous != oldProduct.Hazardous {
+		changedValues["hazardous"] = newProduct.Hazardous
+	}
+	if newProduct.Category != oldProduct.Category {
+		changedValues["category"] = newProduct.Category
+	}
+	if newProduct.Price != oldProduct.Price {
+		changedValues["price"] = newProduct.Price
+	}
+	if newProduct.Desc != oldProduct.Desc {
+		changedValues["desc"] = newProduct.Desc
+	}
+	if newProduct.Slug != oldProduct.Slug {
+		changedValues["slug"] = newProduct.Slug
+	}
+	if newProduct.NameKey != oldProduct.NameKey {
+		changedValues["nameKey"] = newProduct.NameKey
+	}
+	if newProduct.Quantity != oldProduct.Quantity {
+		changedValues["quantity"] = newProduct.Quantity
+	}
+
+	if len(changedValues) == 0 {
+		return nil
+	}
+	changedValues["updatedAt"] = time.Now()
+
+	return changedValues
+}
