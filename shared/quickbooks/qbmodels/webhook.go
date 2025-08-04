b9d177df062c7d5afc75f4ed49d276766ab01054
harshmohansason <harshsason2000@gmail.com>
2025-08-04 08:47:39 +0530

    Removed the QBWebhookPubMessage struct and the method

--- shared/quickbooks/qbmodels/webhook.go
+++ shared/quickbooks/qbmodels/webhook.go
@@ -25,21 +25,3 @@ type Entity struct {
 func (e *Entity) ToBytes() ([]byte, error) {
 	return json.Marshal(e)
 }
-
-// Represents the data payload pushed by quickbooks to the pubsub
-// The subscriber is made to only accept the data field. Gen2 functions 
-// still send this data field instead of sending the raw json bytes when 
-// pushed as a message to the topic so we need this struct to get the data 
-// then have a helper function to convert it to an Entity struct
-type QBWebHookPubMessage struct {
-	Data []byte `json:"data"`
-}
-
-func (qbwkm *QBWebHookPubMessage) ToEntity() (*Entity, error) {
-	var entity Entity
-	err := json.Unmarshal(qbwkm.Data, &entity)
-	if err != nil {
-		return nil, err
-	}
-	return &entity, nil
-}
