15d20fa907456120d4acae05b1d8ecb297c1ef6d
harshmohansason <harshsason2000@gmail.com>
2025-08-13 19:48:03 +0530

    removed FetchAccounts and UpdateAccount constants since the cloud functions were deleted as well

--- shared/constants/function_names.go
+++ shared/constants/function_names.go
@@ -4,8 +4,6 @@ const (
 	ContactUs                        = "contact-us"
 	CreateAccount                    = "create-account"
 	DeleteAccount                    = "delete-account"
-	UpdateAccount                    = "update-account"
-	FetchAccounts                    = "fetch-accounts"
 	PlaceOrder                       = "place-order"
 	UpdateOrder                      = "update-order"
 	DeliverOrder                     = "deliver-order"
