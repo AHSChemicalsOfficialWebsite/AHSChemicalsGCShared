e15b907537ae172eb7ced4f018a869c0e6e657aa
harshmohansason <harshsason2000@gmail.com>
2025-08-30 04:44:12 +0530

    Added a GenerateRandomID function to create unique id's for order generation mostly

--- shared/utils/crypto_utils.go
+++ shared/utils/crypto_utils.go
@@ -1,16 +1,20 @@
-//utils package contains common utility functions that are used across multiple packages in the application
+// utils package contains common utility functions that are used across multiple packages in the application
 package utils
 
 import (
 	"crypto/rand"
 	"encoding/base64"
+	"math/big"
 )
 
-// generateRandomSecret generates a cryptographically secure random string,
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+
+// GenerateRandomSecret generates a cryptographically secure random string,
 // suitable for use as OAuth 'state' parameters, API secrets, etc.
 //
 // Returns:
 //   - string: A URL-safe base64 encoded random secret of 32 bytes (typically ~43 characters).
+//   - error: Any error that may have occurred.
 //
 // Example:
 //   secret := generateRandomSecret()
@@ -20,4 +24,22 @@ func GenerateRandomSecret() (string, error) {
 		return "", err
 	}
 	return base64.URLEncoding.WithPadding(base64.NoPadding).EncodeToString(b), nil
+}
+
+// GenerateRandomID generates a cryptographically secure random string,
+//
+// Returns:
+//   - string: A a string of length 'length' containing only the letters 'a'-'z', 'A'-'Z', and '0'-'9'.
+//	 - error: Any error that may have occurred.
+//
+func GenerateRandomID(length int) (string, error) {
+	id := make([]byte, length)
+	for i := range id {
+		n, err := rand.Int(rand.Reader, big.NewInt(int64(len(letters))))
+		if err != nil {
+			return "", err
+		}
+		id[i] = letters[n.Int64()]
+	}
+	return string(id), nil
 }
