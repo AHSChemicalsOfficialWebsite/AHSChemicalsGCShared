7d116e11a0598360e0c89ae24a419b568eda9b4f
harshmohansason <harshsason2000@gmail.com>
2025-09-06 16:36:55 +0530

    added GetUpdatedProductDetails from services package to not have any import cycles

--- shared/models/product.go
+++ shared/models/product.go
@@ -248,3 +248,64 @@ func (p *Product) GetFormattedTotalNonHazardousWeight() string {
 	}
 	return "N/A"
 }
+
+func GetUpdatedProductDetails(newProduct, oldProduct *Product) map[string]any {
+	if newProduct == nil || oldProduct == nil {
+		return nil
+	}
+
+	changedValues := make(map[string]any)
+
+	if newProduct.IsActive != oldProduct.IsActive {
+		changedValues["isActive"] = newProduct.IsActive
+	}
+	if newProduct.Brand != oldProduct.Brand {
+		changedValues["brand"] = newProduct.Brand
+	}
+	if newProduct.Name != oldProduct.Name {
+		changedValues["name"] = newProduct.Name
+	}
+	if newProduct.SKU != oldProduct.SKU {
+		changedValues["sku"] = newProduct.SKU
+	}
+	if newProduct.Size != oldProduct.Size {
+		changedValues["size"] = newProduct.Size
+	}
+	if newProduct.SizeUnit != oldProduct.SizeUnit {
+		changedValues["sizeUnit"] = newProduct.SizeUnit
+	}
+	if newProduct.PackOf != oldProduct.PackOf {
+		changedValues["packOf"] = newProduct.PackOf
+	}
+	if newProduct.Hazardous != oldProduct.Hazardous {
+		changedValues["hazardous"] = newProduct.Hazardous
+	}
+	if newProduct.Category != oldProduct.Category {
+		changedValues["category"] = newProduct.Category
+	}
+	if newProduct.Price != oldProduct.Price {
+		changedValues["price"] = newProduct.Price
+	}
+	if newProduct.Desc != oldProduct.Desc {
+		changedValues["desc"] = newProduct.Desc
+	}
+	if newProduct.Slug != oldProduct.Slug {
+		changedValues["slug"] = newProduct.Slug
+	}
+	if newProduct.NameKey != oldProduct.NameKey {
+		changedValues["nameKey"] = newProduct.NameKey
+	}
+	if newProduct.Quantity != oldProduct.Quantity {
+		changedValues["quantity"] = newProduct.Quantity
+	}
+	if newProduct.PurchasePrice != oldProduct.PurchasePrice {
+		changedValues["purchasePrice"] = newProduct.PurchasePrice
+	}
+
+	if len(changedValues) == 0 {
+		return nil
+	}
+	changedValues["updatedAt"] = firestore.ServerTimestamp
+
+	return changedValues
+}
