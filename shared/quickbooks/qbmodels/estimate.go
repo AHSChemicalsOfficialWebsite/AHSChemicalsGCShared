2b87d8cf7f47dc99620a43088f5ca5b84eb8168b
harshmohansason <harshsason2000@gmail.com>
2025-09-11 17:08:37 +0530

    Added QBEstimate struct, func to create a new estimate and adding lines. Also added a QBEstimateResponse struct to unmarshal when retreving the response from qb

---
+++ shared/quickbooks/qbmodels/estimate.go
@@ -0,0 +1,54 @@
+package qbmodels
+
+import (
+	"github.com/HarshMohanSason/AHSChemicalsGCShared/shared/models"
+	"github.com/HarshMohanSason/AHSChemicalsGCShared/shared/quickbooks"
+)
+
+type QBEstimate struct {
+	ID             string               `json:"Id"`
+	SyncToken      string               `json:"SyncToken,omitempty"`
+	MetaData       *quickbooks.MetaData `json:"MetaData,omitempty"`
+	CustomerRef    *Reference           `json:"CustomerRef"`
+	TotalAmt       float64              `json:"TotalAmt,omitempty"`
+	Balance        float64              `json:"Balance,omitempty"`
+	TxnDate        string               `json:"TxnDate,omitempty"`
+	ExpirationDate string               `json:"ExpirationDate,omitempty"`
+	Line           []Line               `json:"Line,omitempty"`
+	TxnTaxDetail   *TxnTaxDetail        `json:"TxnTaxDetail,omitempty"`
+	CurrencyRef    *Reference           `json:"CurrencyRef,omitempty"`
+	CustomField    []CustomField        `json:"CustomField,omitempty"`
+}
+
+func NewQBEstimate(order *models.Order) *QBEstimate {
+	QBEstimate := &QBEstimate{
+		CustomerRef: &Reference{
+			Value: order.Customer.ID,
+			Name:  order.Customer.Name,
+		},
+		TotalAmt: order.Total,
+		TxnDate:  order.CreatedAt.Format("2006-01-02"),
+	}
+	QBEstimate.AddLines(order)
+	return QBEstimate
+}
+
+func (i *QBEstimate) AddLines(order *models.Order) {
+	invoiceLines := make([]Line, 0)
+	for _, item := range order.Items {
+		line := Line{
+			DetailType:  "SalesItemLineDetail",
+			Description: item.GetFormattedDescription(),
+			Amount:      item.GetTotalPrice(),
+		}
+		line.SetSalesItemLineDetail(item)
+		line.SalesItemLineDetail.TaxCodeRef.Value = "TAX"
+		invoiceLines = append(invoiceLines, line)
+	}
+	i.Line = invoiceLines
+}
+
+// Wrapper for the api response
+type QBEstimateResponse struct {
+	Estimate *QBEstimate `json:"Estimate"`
+}
