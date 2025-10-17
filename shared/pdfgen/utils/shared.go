ebe0c11f98050388639620428d20996845dd52c0
harshmohansason <harshsason2000@gmail.com>
2025-10-18 00:06:32 +0530

    renamed the function

--- shared/pdfgen/utils/shared.go
+++ shared/pdfgen/utils/shared.go
@@ -1,6 +1,6 @@
 package utils
 
-func CalculateShippingTableCellWidths(cellWidths []float64) float64 {
+func CalculateTableCellWidths(cellWidths []float64) float64 {
 	var totalWidth float64 = 0.0
 	for i := range cellWidths {
 		totalWidth += cellWidths[i]
