6bea4b55aaa02144437460b6bb58d279f93aa3a4
harshmohansason <harshsason2000@gmail.com>
2026-06-11 11:41:10 +0530

    updates

--- models/notification.go
+++ models/notification.go
@@ -1,6 +1,13 @@
 package models
 
+import "encoding/json"
+
 type Notifcation struct {
 	Title string `json:"title"`
 	Body  string `json:"body"`
+}
+
+func (n *Notifcation) ToBytes() []byte {
+	b, _ := json.Marshal(n)
+	return b
 }
