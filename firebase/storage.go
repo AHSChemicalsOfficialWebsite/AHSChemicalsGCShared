b4ab1c1a9bbc60675fd4e7cedecf6df6259830d8
harshmohansason <harshsason2000@gmail.com>
2026-06-11 09:51:47 +0530

    updated

--- firebase/storage.go
+++ firebase/storage.go
@@ -3,6 +3,9 @@ package firebase
 import (
 	"context"
 	"fmt"
+	"io"
+
+	"cloud.google.com/go/storage"
 )
 
 func UploadDataToStorage(ctx context.Context, path string, data []byte, metaData map[string]string) error {
@@ -25,4 +28,34 @@ func UploadDataToStorage(ctx context.Context, path string, data []byte, metaData
 	}
 
 	return nil
+}
+
+func DownloadDataFromStorage(ctx context.Context, path string) ([]byte, error) {
+	bucket, err := StorageClient.Bucket(StorageBucket)
+	if err != nil {
+		return nil, err
+	}
+	reader, err := bucket.Object(path).NewReader(ctx);
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read from Cloud Storage: %w", err)
+	}
+
+	return data, nil
+}
+
+func GetStorageObjectMetadata(ctx context.Context, path string) (*storage.ObjectAttrs, error) {
+    bucket, err := StorageClient.Bucket(StorageBucket)
+    if err != nil {
+        return nil, err
+    }
+    attrs, err := bucket.Object(path).Attrs(ctx)
+    if err != nil {
+        return nil, err
+    }
+    return attrs, nil
 }
